364945fb95d37445e83327703133f9e993cd236a
rngrhn4114 <130418820+rngrhn4114@users.noreply.github.com>
2024-09-30 01:05:02 +0900

    Create ReviewActivity.kt

---
+++ ReviewActivity.kt
@@ -0,0 +1,40 @@
+import android.content.Intent
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+
+class ReviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review)
+
+        val homeButton = findViewById<LinearLayout>(R.id.homeButton)
+        val questionButton = findViewById<LinearLayout>(R.id.questionButton)
+        val friendsButton = findViewById<LinearLayout>(R.id.friendsButton)
+        val profileButton = findViewById<LinearLayout>(R.id.profileButton)
+
+        homeButton.setOnClickListener {
+            // Home 버튼 클릭 시 처리할 작업
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        questionButton.setOnClickListener {
+            // Question 버튼 클릭 시 처리할 작업
+            val intent = Intent(this, QuestionActivity::class.java)
+            startActivity(intent)
+        }
+
+        friendsButton.setOnClickListener {
+            // Friends 버튼 클릭 시 처리할 작업
+            val intent = Intent(this, FriendsActivity::class.java)
+            startActivity(intent)
+        }
+
+        profileButton.setOnClickListener {
+            // Profile 버튼 클릭 시 처리할 작업
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
